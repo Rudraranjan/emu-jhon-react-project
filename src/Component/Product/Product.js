42d77ff9042c44d156da00a8d0de1ed7f5196ea9
Rudraranjan Thakur <rudraranjan.thakur@gmail.com>
2021-09-29 19:37:08 +0530

    commit with search and rating system

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
+import Rating from 'react-rating';
 
 const Product = (props) => {
     // console.log(props.product);
-    const {name, price, seller,stock, img} = props.product;
+    const {name, price, seller,stock, img, star} = props.product;
     const element = <FontAwesomeIcon icon={faShoppingCart} />
     return (
         <div className="product">
@@ -17,6 +18,14 @@ const Product = (props) => {
                 <p>Sell By : {seller}</p>
                 <h3>Price: {price}</h3>
                 <p>Only {stock} items left</p>
+                <Rating
+                 initialRating={star}
+                 emptySymbol="far fa-star icon"
+                 fullSymbol="fas fa-star icon"                
+                readonly>                
+                  </Rating>
+                <br />
+                <br />
                      <button onClick={() => props.handelProductAddtoCart(props.product)} 
                     className="btn-regular">
                     {element} Add to Cart
--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -9,23 +10,39 @@ const Shop = () => {
 
     const [cart, setCart] = useState([]);
 
+    const[displayProduct, setDisplayProduct] = useState([]);
+
     useEffect(()=> {
         fetch('./products.JSON')
         .then(res => res.json())
-        .then(data => setProducts(data));
+        .then(data => {setProducts(data);
+        setDisplayProduct(data)
+    });
+                       
 
     },[])
 
     const handelProductAddtoCart = (product) => {
             const newCart = [...cart, product]
             setCart(newCart);
+            addToDb(product.key);
+    }
+
+    const seachHandeler = event =>{
+        const inputValue = event.target.value;
+        const matchProducts = products.filter(product => product.name.toLowerCase().includes(inputValue.toLowerCase()));
+        setDisplayProduct(matchProducts);
     }
     return (
-        <div className="shop-container">
+       <div>
+           <div className="search-box">
+               <input onChange={seachHandeler} type="text" placeholder="Search Here" />
+           </div>
+            <div className="shop-container">
             <div className="product-container">
                 <h3>Product : {products.length}</h3>
                 {
-                    products.map(product => <Product 
+                    displayProduct.map(product => <Product 
                         key = {product.key}
                          product={product} 
                          handelProductAddtoCart ={handelProductAddtoCart}>
@@ -34,9 +51,9 @@ const Shop = () => {
             </div>
             <div className="cart-container">
                <Cart cart={cart}></Cart>
-            </div>
-            
+            </div>            
         </div>
+       </div>
     );
 };
 
